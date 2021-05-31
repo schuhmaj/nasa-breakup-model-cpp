e1b32b0d8e101cf800026f7b050608dd17a3749b
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2021-05-31 15:46:36 +0200

    Added some comments + future TODOs

--- src/simulation/BreakupFactory.h
+++ src/simulation/BreakupFactory.h
@@ -8,14 +8,16 @@
 #include "Collision.h"
 
 /**
- * Interface to easily create a Breakup Event.
- * Based on a modified Builder Pattern.
+ * Interface to easily create a Breakup Simulation.
  */
 class BreakupFactory {
 
     std::shared_ptr<InputReader> _inputReader;
 
 public:
+
+    explicit BreakupFactory(std::shared_ptr<InputReader> &inputReader)
+            : _inputReader{inputReader} {}
 
     /**
      * Adds an input source for the satellites.
@@ -24,9 +26,18 @@ public:
      */
     BreakupFactory &changeInputSource(const std::shared_ptr<InputReader> &inputReader);
 
-
+    /**
+     * Creates a new Explosion Breakup Simulation with the given input.
+     * TODO Add error handling
+     * @return Breakup Simulation
+     */
     Breakup getExplosion();
 
+    /**
+     * Creates a new Collision Breakup Simulation with the given input.
+     * TODO Add error handling, e. g. no collision can occur when there is only 1 satellite in the collection!
+     * @return Breakup Simulation
+     */
     Breakup getCollision();
 
 
