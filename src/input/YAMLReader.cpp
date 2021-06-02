4ba8e24847576c39c07e7a9f43a079556bf26598
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2021-06-02 18:19:00 +0200

    Integrated YAML input and connected it to the simulation, added example data.yaml

--- src/input/YAMLReader.cpp
+++ src/input/YAMLReader.cpp
@@ -1,6 +1,42 @@
 #include "YAMLReader.h"
 
+#include <iostream>
+
 SatelliteCollection YAMLReader::getSatelliteCollection() {
-    //TODO Next step
-    return SatelliteCollection();
+    SatelliteCollection satelliteCollection{};
+    SatelliteBuilder satelliteBuilder{};
+
+    YAML::Node file = YAML::LoadFile(_filename);
+
+    if (file["satellites"]) {
+        YAML::Node satellites{file["satellites"]};
+        for (auto satNode : satellites) {
+            try {
+                satelliteCollection.push_back(parseSatellite(satelliteBuilder, satNode));
+            } catch (const std::exception& e) {
+                std::cerr << e.what();
+            }
+        }
+    }
+    return satelliteCollection;
+}
+
+Satellite YAMLReader::parseSatellite(SatelliteBuilder &satelliteBuilder, const YAML::Node& node) {
+    satelliteBuilder.reset();
+    if (node["id"]) {
+        satelliteBuilder.setID(node["id"].as<unsigned long>());
+    }
+    if (node["name"]) {
+        satelliteBuilder.setName(node["name"].as<std::string>());
+    }
+    if (node["type"]) {
+        //TODO
+    }
+    if (node["mass"]) {
+        satelliteBuilder.setMass(node["mass"].as<double>());
+    }
+    if (node["velocity"]) {
+        satelliteBuilder.setVelocity(node["velocity"].as<std::array<double, 3>>());
+    }
+    return satelliteBuilder.getResult();
 }
--- src/input/YAMLReader.h
+++ src/input/YAMLReader.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #include <fstream>
+#include <exception>
 #include "yaml-cpp/yaml.h"
 #include "InputReader.h"
 #include "model/Satellite.h"
 #include "model/SatelliteCollection.h"
+#include "model/SatelliteBuilder.h"
 
 class YAMLReader : public InputReader {
 
@@ -13,6 +15,10 @@ public:
     using InputReader::InputReader;
 
     SatelliteCollection getSatelliteCollection() override;
+
+private:
+
+    static Satellite parseSatellite(SatelliteBuilder &satelliteBuilder, const YAML::Node& node);
 
 };
 
