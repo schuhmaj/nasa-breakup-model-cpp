1de4c53b2fed6cb793426951e006568366ed20e2
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2021-06-30 15:16:19 +0200

    CSV Reader Test

--- test/input/CSVReaderTest.cpp
+++ test/input/CSVReaderTest.cpp
@@ -1,7 +1,8 @@
 #include "gtest/gtest.h"
 
 #include <string>
-#include <vector>
+#include <array>
+#include <tuple>
 #include "input/CSVReader.h"
 #include "model/Satellite.h"
 
@@ -28,11 +29,24 @@ TEST_F(CSVReaderTest, readHeader) {
 }
 
 TEST_F(CSVReaderTest, readLines) {
+    auto expectedTuple01 = std::make_tuple("SL-1 R/B", "1957-001A", 1, SatType::ROCKET_BODY, "D", "CIS", "1957-10-04", "TYMSC",
+                                   "1957-12-01", 96.19, 65.10, 938.0, 214.0, 20.4200, "", "EA", "IMP");
+    auto expectedTuple02 = std::make_tuple("SPUTNIK 1", "1957-001B", 2, SatType::SPACECRAFT, "D", "CIS", "1957-10-04", "TYMSC",
+                                   "1958-01-03", 96.10, 65.00, 1080.0, 64.0, 0.0, "", "EA", "IMP");
+    auto expectedTuple03 = std::make_tuple("SPUTNIK 2", "1957-002A", 3, SatType::SPACECRAFT, "D", "CIS", "1957-11-03", "TYMSC",
+                                   "1958-04-14", 103.74, 65.33, 1659.0, 211.0, 0.0800, "", "EA", "IMP");
+    auto expectedTuple04 = std::make_tuple("EXPLORER 1", "1958-001A", 4, SatType::SPACECRAFT, "D", "US", "1958-02-01", "AFETR",
+                                   "1970-03-31", 88.48, 33.15, 215.0, 183.0, 0.0, "", "EA", "IMP");
+
     CSVReader<std::string, std::string, size_t,
             SatType, std::string, std::string, std::string, std::string, std::string,
             double, double, double, double, double,
             std::string, std::string, std::string> satcatReader{"resources/testShrinkedSatcat.csv", true};
 
-    auto lines = satcatReader.getLines();
+    auto actualTuple = satcatReader.getLines();
 
+    ASSERT_EQ(actualTuple[0], expectedTuple01);
+    ASSERT_EQ(actualTuple[1], expectedTuple02);
+    ASSERT_EQ(actualTuple[2], expectedTuple03);
+    ASSERT_EQ(actualTuple[3], expectedTuple04);
 }
