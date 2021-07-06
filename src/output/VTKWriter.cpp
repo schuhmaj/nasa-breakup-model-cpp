5c362aa6363623432df4f65391f5c5b83a649d8b
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2021-07-06 18:49:04 +0200

    implemented VTKWriter

--- src/output/VTKWriter.cpp
+++ src/output/VTKWriter.cpp
@@ -1,6 +1,32 @@
 #include "VTKWriter.h"
 
 void VTKWriter::printResult(const std::vector<Satellite> &satelliteCollection) {
+    this->printHeader(satelliteCollection.size());
 
+    this->printProperty<double>("mass", &Satellite::getMass, satelliteCollection);
+    this->printProperty<double>("area", &Satellite::getArea, satelliteCollection);
+    this->printProperty<double>("area-to-mass", &Satellite::getAreaToMassRatio, satelliteCollection);
+    this->printProperty<double>("velocity", &Satellite::getVelocity, satelliteCollection);
+    this->printProperty<double>("position", &Satellite::getPosition, satelliteCollection);
+
+    this->printFooter();
+}
+
+void VTKWriter::printHeader(size_t size) {
+    _logger->info(R"(<?xml version="1.0" encoding="UTF-8" standalone="no" ?>)");
+    _logger->info(R"(<VTKFile byte_order="LittleEndian" type="UnstructuredGrid" version="0.1">)");
+    _logger->info(R"(  <UnstructuredGrid>)");
+    _logger->info(R"(    <Piece NumberOfCells="0" NumberOfPoints="{}">)", size);
+    _logger->info(R"(      <PointData>)");
+}
+
+void VTKWriter::printFooter() {
+    _logger->info(R"(      </Points>)");
+    _logger->info(R"(      <Cells>)");
+    _logger->info(R"(        <DataArray Name="types" NumberOfComponents="0" format="ascii" type="Float32"/>)");
+    _logger->info(R"(      </Cells>)");
+    _logger->info(R"(    </Piece>)");
+    _logger->info(R"(  </UnstructuredGrid>)");
+    _logger->info(R"(</VTKFile>)");
 }
 
--- src/output/VTKWriter.h
+++ src/output/VTKWriter.h
@@ -3,19 +3,22 @@
 #include "output/OutputWriter.h"
 
 #include <string>
+#include <functional>
 #include "model/Satellite.h"
 #include "spdlog/async.h"
 #include "spdlog/sinks/basic_file_sink.h"
 #include "spdlog/fmt/ostr.h"
 
+/**
+ * Creates vtk file of the given input.
+ */
 class VTKWriter : public OutputWriter {
 
     std::shared_ptr<spdlog::logger> _logger;
 
 public:
 
-    //TODO Change default filename
-    VTKWriter() : VTKWriter("todo.vtk") {}
+    VTKWriter() : VTKWriter("breakupResult.vtu") {}
 
     explicit VTKWriter(const std::string &filename)
             : _logger{spdlog::basic_logger_mt<spdlog::async_factory>("VTKWriter_" + filename, filename, true)} {
@@ -23,5 +26,55 @@ public:
     }
 
     void printResult(const std::vector<Satellite> &satelliteCollection) override;
+
+private:
+
+    /**
+     * Prints a property of the points.
+     * @tparam T - the number type (double, float, int, etc.)
+     * @param name - name of the property, e.g. mass
+     * @param property - the property (normally a getter of an satellite)
+     * @param satelliteCollection - the data
+     */
+    template<typename T>
+    void printProperty(const std::string &name, const std::function<T(const Satellite &satellite)> &property,
+                       const std::vector<Satellite> &satelliteCollection) {
+        _logger->info(R"(        <DataArray Name="{}" NumberOfComponents="1" format="ascii" type="Float32">)", name);
+        for (const auto &sat : satelliteCollection) {
+            _logger->info("          {}", property(sat));
+        }
+        _logger->info(R"(        </DataArray>)");
+    }
+
+    /**
+     * Prints a property of the points.
+     * @tparam T - the number type (double, float, int, etc.)
+     * @param name - name of the property, e.g. mass
+     * @param property - the property (normally a getter of an satellite), here return value cartesian vector
+     * @param satelliteCollection - the data
+     * @related I do not use the >> overload because of ADL (https://en.cppreference.com/w/cpp/language/adl)
+     */
+    template<typename T>
+    void printProperty(const std::string &name, const std::function<std::array<T, 3>(const Satellite &satellite)> &property,
+                       const std::vector<Satellite> &satelliteCollection) {
+        _logger->info(R"(<DataArray Name="{}" NumberOfComponents="3" format="ascii" type="Float32">)", name);
+        for (const auto &sat : satelliteCollection) {
+            const auto &array = property(sat);
+            _logger->info("          {} {} {}", array[0], array[1], array[2]);
+        }
+        _logger->info(R"(</DataArray>)");
+    }
+
+    /**
+     * Prints the Header of the VTK file.
+     * @param size - the number of points
+     */
+    void printHeader(size_t size);
+
+    /**
+     * Prints the Footer of the VTK file.
+     */
+    void printFooter();
+
 
 };
