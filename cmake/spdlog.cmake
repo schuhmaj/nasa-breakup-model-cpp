7e61a17de496462ad855644c7b59d1faf2aff0f9
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2021-07-26 17:52:19 +0200

    name change

--- cmake/spdlog.cmake
+++ cmake/spdlog.cmake
@@ -4,8 +4,10 @@ include(FetchContent)
 FetchContent_Declare(spdlog
         URL https://github.com/gabime/spdlog/archive/refs/tags/v1.8.5.zip
         )
+
 # Disable stuff we don't need
 option(SPDLOG_BUILD_EXAMPLE "" OFF)
 option(SPDLOG_BUILD_TESTS "" OFF)
 option(SPDLOG_INSTALL "" OFF)
+
 FetchContent_MakeAvailable(spdlog)
