3012e14d0783f4b057f82ea129c0f5a1a53e83d3
Jonas Schuhmacher <jonas.schuhmacher@tum.de>
2021-09-05 19:02:59 +0200

    added Delta-V Distribution

--- script/breakupCompareScript.py
+++ script/breakupCompareScript.py
@@ -24,10 +24,29 @@ def compare_histogram_am(data_cpp, data_python, title):
     plt.hist(data_cpp["A/M [m^2/kg]"], bins, alpha=0.5, log=True, label="C++", color='blue')
     plt.hist(data_python["A/M [m^2/kg]"], bins, alpha=0.5, log=True, label="Python", color='red')
     plt.legend(loc='upper right')
-    plt.xlabel("$L_c$ in [m]")
+    plt.xlabel("$A/M$ in [m^2/kg]")
     plt.ylabel("Number")
     plt.title("$A/M$ Distribution C++ vs. Python for " + title)
     plt.savefig("compare_histogram_am_CPP_vs_Python", dpi=300)
+    plt.close(fig)
+
+
+def compare_histogram_dv(data_cpp, data_python, title):
+    fig = plt.figure(figsize=(6, 4), dpi=300)
+    bins = np.linspace(0, 10000, 100)
+    velocity_cpp = np.array([np.fromstring(ele[1:-1], sep=' ') for ele in data_cpp["Ejection Velocity [m/s]"].to_numpy()])
+    velocity_python = np.array([np.fromstring(ele[1:-1], sep=' ') for ele in data_python["Ejection Velocity [m/s]"].to_numpy()])
+
+    scalar_cpp = np.linalg.norm(velocity_cpp, axis=1)
+    scalar_python = np.linalg.norm(velocity_python, axis=1)
+
+    plt.hist(scalar_cpp, bins, alpha=0.5, log=True, label="C++", color='blue')
+    plt.hist(scalar_python, bins, alpha=0.5, log=True, label="Python", color='red')
+    plt.legend(loc='upper right')
+    plt.xlabel("$\Delta V$ in [m/s]")
+    plt.ylabel("Number")
+    plt.title("$\Delta V$ Distribution C++ vs. Python for " + title)
+    plt.savefig("compare_histogram_dv_CPP_vs_Python", dpi=300)
     plt.close(fig)
 
 
@@ -41,6 +60,7 @@ def main():
 
     compare_histogram_lc(df_cpp, df_python, plot_name)
     compare_histogram_am(df_cpp, df_python, plot_name)
+    compare_histogram_dv(df_cpp, df_python, plot_name)
 
 
 if __name__ == '__main__':
